710a97b443e071fc2aa0a53a049446c75b61a246
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2025-08-26 20:45:06 +0500

    majority element brute force approach solved

--- Lecture 11/majority_element_BruteForce.cpp
+++ Lecture 11/majority_element_BruteForce.cpp
@@ -1,1 +1,32 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+//function to find majority element in an array
+int majority_element(vector<int> nums){
+    int n = nums.size();
+
+    // Pick each element one by one
+    for(int val: nums){
+        int freq = 0;
+
+        // Count how many times 'val' occurs
+        for (int el: nums){
+            if(el == val){
+                freq ++;
+            }
+        }
+        // Check majority condition
+        if(freq > n/2){
+            return val;
+        }
+    }
+    // No majority element
+    return -1;
+}
+
+int main(){
+    vector<int> nums = {1,2,2,2,1};
+    cout << "Majority Element: " << majority_element(nums) << endl;
+    return 0;
+}
